a8c8e9f8110eaccc93caf40198cf0928a0ce35fc
BTH-IT <hungbienthanh@gmail.com>
2023-08-15 22:55:50 +0700

    add tailwindcss and icons library

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { RectangleStackIcon } from "@heroicons/react/24/solid";
+
+const data = [
+  {
+    title: "Live Visual Builder",
+    icon: <RectangleStackIcon className="h-6 w-6 text-gray-500" />,
+  },
+];
+
 function App() {
   return <></>;
 }
